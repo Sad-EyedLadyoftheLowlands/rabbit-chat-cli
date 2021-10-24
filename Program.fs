e7397b9ab9ee4572fbb4adaee58d22f64df0aa8d
Dylan Baird <Baird.Dylan@gmail.com>
2021-10-24 19:21:58 -0400

    Testing http interaction. Can get all messages and cast them as Message type. Very basic, but at least works-ish.

--- Program.fs
+++ Program.fs
@@ -1,10 +1,25 @@
 open System
 open System.Threading
 open System.Text
+open System.Text.Json
+// open System.Net.Http
 open RabbitMQ.Client
 open RabbitMQ.Client.Events
+open FSharp.Data
+
+type Message = {
+    messageId: int;
+    rabbitUserId: int;
+    timeSent: DateTime;
+    messageContent: string;
+    roomId: int;
+}
+
+let printWelcome () =
+    printfn "%s" "********** WELCOME TO RABBIT CHAT **********"
 
 let setup () = 
+    printWelcome()
     printfn "%s" "Setting up application"
 
 let handleInput input = 
@@ -14,10 +29,17 @@ let handleInput input =
 
 let handleShutdown () = Environment.Exit(0)
 
+let getRoomMessages () =
+    Http.RequestString("http://localhost:5000/api/message/4")
+    |> JsonSerializer.Deserialize<Message[]>
+
+let listFriends () = printfn "%s" "list"
+
 let handleCommand input =
     printfn "%s" "processing command"
     match input with
     | "~quit" -> handleShutdown()
+    | "~friends" -> listFriends()
     | _ -> printfn "%s" input
 
 let rec readInput () =
@@ -29,40 +51,10 @@ let rec readInput () =
 
     readInput()
 
-let rec readInputAsync () = async {
-    printfn "%s" "waiting"
-    let input = Console.ReadLine()
-    match input with
-    | input when input.StartsWith("~") -> handleCommand input 
-    | _ -> handleInput input
-
-    readInputAsync() 
-}
-
-let processCommand command =
-    command
-
-// RABBIT MQ TESTS
-
-let produceMessage hostName exchange routingKey (token : CancellationTokenSource) = async {
-    let factory = ConnectionFactory(HostName = hostName, UserName = "guest", Password = "guest")
-    use connection = factory.CreateConnection()
-    use channel = connection.CreateModel()
-    channel.ExchangeDeclare(exchange = exchange, ``type`` = ExchangeType.Fanout, durable = false) // , auto
-
-    let rand = Random()
-
-    while not token.IsCancellationRequested do
-        let message = sprintf "%f" (rand.NextDouble())
-        let body = Encoding.UTF8.GetBytes(message)
-        printfn "publish: %s" message
-        channel.BasicPublish(exchange = exchange, routingKey = routingKey, body = ReadOnlyMemory<byte>(body))
-        Thread.Sleep(500)
-
-    printfn "%s" "Closing connection"
-}
-
-let produceMessageSync hostName exchange routingKey (token : CancellationTokenSource) = 
+let produceMessage (token : CancellationTokenSource) = 
+    let hostName = "localhost"
+    let exchange = "rabbit-test"
+    let routingKey = ""
     let factory = ConnectionFactory(HostName = hostName, UserName = "guest", Password = "guest")
     use connection = factory.CreateConnection()
     use channel = connection.CreateModel()
@@ -79,35 +71,16 @@ let produceMessageSync hostName exchange routingKey (token : CancellationTokenSo
 
 [<EntryPoint>]
 let main argv =
-    let host = "localhost"
-    let exchange = "rabbit-test"
-    let routingKey = ""
-
-    //  async {
-    //      let token = new CancellationTokenSource()
-    //      token.CancelAfter 5000
-    //      produceMessage host exchange routingKey token
-    //  } 
-
-    //  readInputAsync()
-    //  Console.ReadKey()
-
     // let token = new CancellationTokenSource()
     // token.CancelAfter 5000
-    // produceMessageSync host exchange routingKey token
+    // produceMessage token
 
-    async {
-        let token = new CancellationTokenSource()
-        token.CancelAfter 5000
-        seq {
-            (produceMessage host exchange routingKey token);
-            (readInputAsync()); 
-        }
-        |> Async.Parallel
-        |> Async.RunSynchronously
-    } 
-    |> Async.RunSynchronously
+    let res = getRoomMessages()
+    
+    res
+    |> Array.take 10
+    |> Array.iter (fun message -> printfn "%s" message.messageContent)
 
-    // setup()
-    // readInput()
+    setup()
+    readInput()
     0 
